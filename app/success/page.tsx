521e140d54531cf6e7514185e236b3429907b46f
melatoninastudio <domininsite@gmail.com>
2026-04-15 06:58:39 +0200

    force static success page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,6 +1,3 @@
-import { Suspense } from "react";
-import SuccessPageClient from "@/components/SuccessPageClient";
-
 export default function SuccessPage() {
   return <main style={{ padding: 24 }}>Success static test</main>;
 }
